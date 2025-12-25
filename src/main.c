3cf879ad0cda03b7e0064e71a5cb7b895d1d7e9d
Jonatan Wincent <jonatan.wincent@proton.me>
2025-12-25 22:42:53 +0100

    Config file as command line argument

--- src/main.c
+++ src/main.c
@@ -1,47 +1,26 @@
-#define _POSIX_C_SOURCE 200809L
-
 #include "config.h"
 #include "menu.h"
 #include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-char *
-get_config_path()
+int
+main(const int argc, const char **argv)
 {
-    const char *file = "jwmenu/config.toml";
-
-    const char *xdg = getenv("XDG_CONFIG_HOME");
-    if (!xdg) {
-        error("get_config_path: XDG_CONFIG_HOME is not set", 0);
+    if (argc != 2) {
+        printf("ERROR: Invalid arguments, usage:\n");
+        printf("\tjwmenu 'path/to/config.toml'\n");
+        exit(EXIT_FAILURE);
     }
 
-    size_t len = strlen(xdg) + 1 + strlen(file) + 1;
-
-    char *path = safe_calloc(1, len);
-
-    snprintf(path, len, "%s/%s", xdg, file);
+    const char *path = argv[1];
 
     if (access(path, R_OK) != 0) {
         error("Cannot access config file: ", path);
     }
 
-    return path;
-}
-
-int
-main(void)
-{
-    char *path = "config.toml";
-    // char *path = get_config_path();
-    if (!path) {
-        return 1;
-    }
-
     struct config *conf = load_config(path);
-    // free(path);
 
     run_menu(conf->menu_items, conf->item_count);
 
